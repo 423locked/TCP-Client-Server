fc0d2e0eb119e162adb6fb7c136a7ed8d9b6ab0c
Yaroslav Prishedko <yaroslav05042005@gmail.com>
2022-02-06 17:56:46 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp22/Server.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Linq;
+using System.Threading;
+
+namespace TCPServer
+{
+    public class Server
+    {
+        public static readonly int Port = 8888;
+        public static readonly string Localhost = "127.0.0.1";
+        private static TcpListener server;
+
+        public Server()
+        {
+
+        }
+        public void Start()
+        {
+            try
+            {
+                IPAddress localhost = IPAddress.Parse(Localhost);
+                server = new TcpListener(localhost, Port);
+
+                server.Start();
+                Console.WriteLine("Waiting for connections...");
+                
+                while(true)
+                {
+                    TcpClient client = server.AcceptTcpClient();
+                    Client clientObject = new(client);
+
+                    // создаем новый поток для обслуживания нового клиента
+                    Thread clientThread = new(new ThreadStart(clientObject.Process));
+                    clientThread.Start();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (server != null)
+                    server.Stop();
+            }
+        }
+    }
+}
---
+++ TCPClient/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+using TCPServer;
+
+namespace TCPClient
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Username: ");
+            string username = Console.ReadLine();
+            TcpClient client = new();
+
+            try
+            {
+                client = new TcpClient(Server.Localhost, Server.Port);
+                NetworkStream stream = client.GetStream();
+
+                while (true)
+                {
+                    // sending
+                    Console.Write(username + ": ");
+                    string message = Console.ReadLine();
+                    message = $"{username}: {message}";
+                    byte[] data = Encoding.Unicode.GetBytes(message);
+                    stream.Write(data, 0, data.Length);
+
+                    // receiving
+                    data = new byte[64];
+                    StringBuilder builder = new StringBuilder();
+                    int bytes;
+                    do
+                    {
+                        bytes = stream.Read(data, 0, data.Length);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (stream.DataAvailable);
+
+                    message = builder.ToString();
+                    Console.WriteLine($"Сервер: {message}");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                client.Close();
+            }
+        }
+    }
+}
